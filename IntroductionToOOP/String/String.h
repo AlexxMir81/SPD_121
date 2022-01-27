29ee3c2eba263b76d6c8ff372797553521e9ba12
Miro_up10@itstepru.onmicrosoft.com <Miro_up10@itstepru.onmicrosoft.com>
2022-01-27 21:08:57 +0300

    HomeWork

---
+++ IntroductionToOOP/String/String.h
@@ -0,0 +1,44 @@
+#pragma once
+#include<iostream>
+#include<Windows.h>
+using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+
+int StringLength(const char* str);
+
+/// ///////////////////////////////////////////////////////////////////////////////////////////////
+/// ////////////////////////    CLASS DECLARACTION (ОБЪЯВЛЕНИЕ КЛАССА)               //////////////
+/// ///////////////////////////////////////////////////////////////////////////////////////////////
+class String;
+String operator+(const String& left, const String& right);
+
+class String
+{
+	int size;	//Размер строки
+	char* str;	//Указатель на строку в динамической памяти
+public:
+	int get_size()const;
+	const char* get_str()const;
+	char* get_str();
+
+	//     Constructors:
+	explicit String(int size = 80);
+	String(const char* str);
+	String(const String& other);
+	~String();
+	//					Operators:
+	String& operator=(const String& other);
+
+	String& operator+=(const String& other);
+	const char& operator[](int i)const;
+	char& operator[](int i);
+
+	//					Methods:
+	void print()const;
+};
+
+/// ///////////////////////////////////////////////////////////////////////////////////////////////
+/// ////////////////////////    CLASS DECLARACTION END (КОНЕЦ ОБЪЯВЛЕНИЯ КЛАССА)     //////////////
+/// ///////////////////////////////////////////////////////////////////////////////////////////////
